95cc5db7958a28f988025cef680c9ce62be3da75
clarkk <clark@electrobeat.dk>
2020-12-10 18:04:14 +0100

    update

--- FS.php
+++ FS.php
@@ -1,11 +1,13 @@
 <?php
 
+namespace FS;
+
 class FS {
 	public function file_mimetype(string $file, bool $is_stream=false): string{
-		$finfo = new finfo(FILEINFO_MIME);
-		$type = $is_stream ? $finfo->buffer($file) : $finfo->file($file);
-		$pos = strpos($type, ';');
-		if($pos !== false){
+		$finfo 	= new \finfo(FILEINFO_MIME);
+		$type 	= $is_stream ? $finfo->buffer($file) : $finfo->file($file);
+		
+		if($pos = strpos($type, ';')){
 			$type = substr($type, 0, $pos);
 		}
 		
