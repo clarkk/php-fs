a9f4ce837ac5b43d7415f6edea9caa1b62f78407
clarkk <clark@electrobeat.dk>
2020-12-11 16:46:54 +0100

    added dir structure

--- File.php
+++ File.php
@@ -2,8 +2,8 @@
 
 namespace FS;
 
-class FS {
-	public function file_mimetype(string $file, bool $is_stream=false): string{
+class File {
+	public function mimetype(string $file, bool $is_stream=false): string{
 		$finfo 	= new \finfo(FILEINFO_MIME);
 		$type 	= $is_stream ? $finfo->buffer($file) : $finfo->file($file);
 		
