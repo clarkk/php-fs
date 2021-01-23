c1a25c7ea6de153ca3a42d30e0a2a39798de3d4e
clarkk <clark@electrobeat.dk>
2021-01-23 19:05:21 +0100

    add purge_dirs

---
+++ Purge_dirs.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace FS;
+
+class Purge_dirs {
+	public function purge(string $dir){
+		while($this->is_empty($dir)){
+			rmdir($dir);
+			
+			$dir = dirname($dir);
+		}
+	}
+	
+	private function is_empty(string $dir): bool{
+		$handle = opendir($dir);
+		while(false !== ($entry = readdir($handle))){
+			if($entry != '.' && $entry != '..'){
+				closedir($handle);
+				
+				return false;
+			}
+		}
+		closedir($handle);
+		
+		return true;
+	}
+}
